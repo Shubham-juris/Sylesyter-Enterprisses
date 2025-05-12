c7e28acf3a9a4029810db10b5d60ae9c674ddf06
It-juris <itjurisnexa@gmail.com>
2025-05-12 11:54:30 +0530

    tailwind and project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 class='text-3xl font-bold underline bg-stone-600 mt-20 py-20 text-center'>
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
